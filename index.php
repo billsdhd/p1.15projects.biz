9b6f21c3abc621a74217be9f80b1135eeba7788b
Bill J <bill@bills-air.home>
2013-09-12 13:20:18 -0400

    Update to show my bio

--- index.php
+++ index.php
@@ -1,1 +1,17 @@
-Welcome to P1 Rev.2
+<!DOCTYPE html>
+	<html>
+        <head>
+ 			<meta charset="UTF-8">
+        	<title>P1</title>
+        </head>
+        <body>
+			<h3>Name : Bill Jung</h3>
+			<br>
+			<b>Courses</b><br>
+			CSCI E-50A,B<br>
+			CSCI E-52<br>
+			<br>
+			<b>Editor</b><br>
+			Aptana
+        </body>
+      </html>
